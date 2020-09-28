0f900196250d7b108180595f63902af57234a017
Ayoung Lee <aylee2008@naver.com>
2020-09-28 22:49:13 +0900

    working on accuracy code

--- computer_vision/driving_area_train/test.py
+++ computer_vision/driving_area_train/test.py
@@ -5,57 +5,68 @@ from scripts.model import *
 import numpy as np
 from torchvision.transforms import transforms
 from scripts.utils import *
+from data_loader.dataset import *
 import time
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--img_path", '-i', type=str, default="/home/snuzero/catkin_ws/src/zero_maker/computer_vision/driving_area_train/data/img/0000.png", help="Path to demo img")
-    parser.add_argument("--weight_path", '-w', type=str, help="Path to model weights")
+    # parser.add_argument("--img_path", '-i', type=str, default="/home/ayoung/catkin_ws/src/zero_maker/computer_vision/data/img/0001.png", help="Path to demo img")
+    parser.add_argument("--weight_path", '-w', type=str, default="/home/ayoung/catkin_ws/src/zero_maker/computer_vision/checkpoints/ldln_ckpt_99.pth", help="Path to model weights")
     parser.add_argument("--band_width", '-b', type=float, default=1.5, help="Value of delta_v")
     parser.add_argument("--visualize", '-v', action="store_true", default=False, help="Visualize the result")
+    parser.add_argument("--test_path", type=str, default="/home/ayoung/catkin_ws/src/zero_maker/computer_vision/test_data/test.txt", help = "test.txt path")
     opt = parser.parse_args()
 
     USE_CUDA = torch.cuda.is_available()
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    img = cv2.imread(opt.img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (640, 480))
-    img_to = transforms.ToTensor()(img)
-    img_to = torch.stack([img_to]).to(DEVICE)
+    test_path = opt.test_path
+
+    dataset = ListDataset(test_path)
+    dataloader = torch.utils.data.DataLoader(dataset, collate_fn=dataset.collate_fn)
+
+    # img = cv2.imread(opt.img_path)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    # img = cv2.resize(img, (640, 480))
+    # img_to = transforms.ToTensor()(img)
+    # img_to = torch.stack([img_to]).to(DEVICE)
 
     model = LaneNet().to(DEVICE)
     model.load_state_dict(torch.load(opt.weight_path))
     model.eval()
 
     start_time = time.time()
-    output = model(img_to)
-    # pix_embedding = output['pix_embedding']
-    # pix_embedding = pix_embedding.detach().cpu().numpy()
-    # embedding = np.transpose(pix_embedding[0],(1,2,0))
-    binary_seg = output['binary_seg']
-    binary_seg_prob = binary_seg.detach().cpu().numpy()
-    binary_seg_pred = np.argmax(binary_seg_prob, axis = 1)[0]
-
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-    seg_img = binary_seg_pred
-
-    # print(time.time() - start_time)
-    # embedding_time = time.time()
-
-    # lane_seg_img = embedding_post_processing(embedding, binary_seg_pred, opt.band_width, 4)
-    # color = np.array([[255, 125, 0], [0, 255, 0], [0, 0, 255], [0, 255, 255], [255, 0, 255]], dtype='uint8')
+    for sample in dataloader:
+        img_to = sample['img'].to(DEVICE)
+        # binLabel = Variable(sample['binLabel'].to(DEVICE), requires_grad= False)
+        output = model(img_to)
+        binary_seg = output['binary_seg']
+        binary_seg_prob = binary_seg.detach().cpu().numpy()
+        # print(np.count_nonzero(binary_seg_prob))
+        binary_seg_pred = np.argmax(binary_seg_prob, axis = 1)[0]
+        # print(np.count_nonzero(binary_seg_pred))
+        # binary_seg_pred = np.array(binary_seg_pred, dtype = np.uint8)
+        # print(np.count_nonzero(binary_seg_pred))
+        binLabel = sample['binLabel'].detach().cpu().numpy()
+        # print(np.count_nonzero(binLabel))
+        binLabel = binLabel[0][0]
+        print(np.count_nonzero(binLabel))
+        print(binLabel)
+        # binLabel = np.array(binLabel, dtype = np.uint8)
+        print(np.count_nonzero(binLabel))
+        # binLabel = np.stack((binLabel), axis = 2)
+        # binary_seg_pred = np.stack((binary_seg_pred), axis = 2)
+        print(binLabel.shape)
+        print(binary_seg_pred.shape)
+        # plt.imshow(binLabel, "gray")
+        plt.imshow(binary_seg_pred, "gray")
+        plt.show()
     
-    # for i, lane_idx in enumerate(np.unique(lane_seg_img)):
-    #     if lane_idx==0:
-    #         continue
-    #     seg_img[lane_seg_img == lane_idx] = color[i-1]
-    print(img.shape)
-    print(seg_img.shape)
-    img = cv2.addWeighted(src1=seg_img, alpha=0.8, src2=img, beta=1., gamma=0.)
+    # img = cv2.addWeighted(src1=seg_img, alpha=0.8, src2=img, beta=1., gamma=0.)
 
-    print(time.time() - embedding_time)
-    cv2.imshow("img", img)
+    # print(time.time() - embedding_time)
+    # cv2.imshow("binary_seg_pred", binary_seg_pred)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 

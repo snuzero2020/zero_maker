efa4988e9243932a6b66679a7c432a81ed422801
mu-crypto <71742167+mu-crypto@users.noreply.github.com>
2020-10-31 15:51:11 +0900

    Update findobstaclepoints.py

--- computer_vision/global_map_generator/scripts/findobstaclepoints.py
+++ computer_vision/global_map_generator/scripts/findobstaclepoints.py
@@ -2,12 +2,13 @@ import cv2
 import numpy as np
 import math
 
-img = cv2.imread('/home/mu/PycharmProjects/birdeye/venv/point_label_img_00005.jpg')
+img = cv2.imread('/home/mu/PycharmProjects/birdeye/venv/global_map.jpg')
 height = list(img.shape)[0]
 width = list(img.shape)[1]
 points = []
-
+Points = []
 obstacle = []
+obstacle1=[]
 print(height, width)
 
 def generateCircle(radius, center):
@@ -25,13 +26,24 @@ for i in range(height)[0:height:int(height/4)]:
         points.append([i, j])
         print(points)
         for pts in generateCircle(15, [i, j]):
-            if np.any(pts) and pts[0]*pts[1] >=0 and pts[0]+pts[1] >=0:
-                obstacle.append(pts)
-
+            if np.any(img[pts[0]][pts[1]]) and pts[0]*pts[1] >=0 and pts[0]+pts[1] >=0 and [i,j] not in obstacle:
+                obstacle.append([i,j])
             else:
                 continue
+for k in range(len(points)):
+    if points[k] in obstacle:
+        Points.append(1)
+    else:
+        Points.append(0)
+
 
 print("Points")
 print(points)
+print('\n')
+
 print("Obstacles")
+
 print(obstacle)
+print(len(obstacle))
+print('\n')
+print(Points)

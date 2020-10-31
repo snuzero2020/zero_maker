def73ad7b89d60e610fc03d1725bd90c3c99e0b1
Lee-JungMo <ljm9906230623@gmail.com>
2020-10-31 17:08:36 +0900

    planner pub add

--- pp/path_planning/src/planning.cpp
+++ pp/path_planning/src/planning.cpp
@@ -8,11 +8,6 @@
 
 #define boundary_distance 1.0
 #define global_point_distance 1.0    
-        
-#define pixel_distance 0.01
-#define random_distance 0.1
-#define choose_parent_distance 0.1
-#define rewire_distance 0.1
 
 using namespace std;
 
@@ -26,35 +21,21 @@ struct Point {
     Point* parrent_point = NULL;
 };
 
-struct point {
-    int x = 0;
-    int y = 0;
-    int cost = 0;
-    Point* parrent_point = NULL;
-    Point* children_point = NULL;
-};
-
-
 class Planning{
     private:
         ros::NodeHandle nh;
-        ros::Subscriber global_map_sub;
-        ros::Publisher local_path_pub;
+        ros::Subscriber start_goal_obstacle_point_sub;
+        ros::Publisher global_path_pub;
 
         Point global_points[16];
         int start_point_index;
         int goal_point_index;
         vector<int> global_path;  
 
-        int current_map [ int( (3 * global_point_distance + 2 * boundary_distance)/pixel_distance ) ][ int( (3 * global_point_distance + 2 * boundary_distance)/pixel_distance ) ]; 
-        int h = int( (3 * global_point_distance + 2 * boundary_distance)/pixel_distance );
-        int w = int( (3 * global_point_distance + 2 * boundary_distance)/pixel_distance );
-
     public:
         Planning(){
-            global_map_sub = nh.subscribe("global_map", 2, &Planning::GlobalMapCallback, this);
-            local_path_pub = nh.advertise<std_msgs::UInt32>("path",2);
-
+            global_path_pub = nh.advertise<nav_msgs::OccupancyGrid>("path",2);
+            start_goal_obstacle_point_sub = nh.subscribe("obstacle_point", 2, &Planning::StartGoalObstaclePointCallback, this);
             for (int i = 0; i < 16; ++i){
                 global_points[i].x = (i % 4) * global_point_distance + boundary_distance;
                 global_points[i].y = (i / 4) * global_point_distance + boundary_distance;
@@ -151,16 +132,6 @@ class Planning{
             calculate_global_path();
         }
 
-        void GlobalMapCallback(const nav_msgs::OccupancyGrid & map){
-
-            for (int i = 0; i < h; i++){
-                for (int j = 0; j < w; j++){
-                    current_map[i][j] =  map.data[j*w+i];
-                }
-            }
-            
-        }
-
         void calculate_global_path(){
 
             int current_cost = 0;
@@ -181,26 +152,9 @@ class Planning{
                         }
                     }
                 }
-/*
-                for (int i = 12; i < 16; ++i){
-                    cout << global_points[i].cost << " ";
-                }
-                cout << "\n" << endl;
-                for (int i = 8; i < 12; ++i){
-                    cout << global_points[i].cost << " ";
-                }
-                cout << "\n" << endl;
-                for (int i = 4; i < 8; ++i){
-                    cout << global_points[i].cost << " ";
-                }
-                cout << "\n" << endl;
-                for (int i = 0; i < 4; ++i){
-                    cout << global_points[i].cost << " ";
-                }
-                cout << "\n" << endl;
+
                 current_cost = current_cost + 1;
-                cout << "\n" << endl;
-*/
+
             }
 
             Point* current_point = global_points + goal_point_index;
@@ -209,62 +163,29 @@ class Planning{
                 current_point = current_point -> parrent_point;
             }
             global_path.push_back(start_point_index);
-/*
-            for (vector<int>::size_type i = 0; i < global_path.size(); ++i){
-                cout << global_path[i] << endl;
+
+        }
+
+        void publish_path(){
+
+            nav_msgs::OccupancyGrid msg;
+
+            for (int i = global_path.size() - 1; i > -1; i = i - 1){
+                //cout << global_points[global_path[i]].x << " " << global_points[global_path[i]].y << endl;
+                msg.data.push_back(global_points[global_path[i]].x);
+                msg.data.push_back(global_points[global_path[i]].y);
             }
-*/ 
+
+            global_path_pub.publish(msg);
+
         }
 
-        void rrt_star(){
-            cout << 1 << endl;
-            //for (int i = global_path.size(); i > 0; i = i - 1){
+        void StartGoalObstaclePointCallback (const nav_msgs::OccupancyGrid & msg){
 
-                //cout << i << endl;
-
-                //point local_start;
-                //point local_goal;
-                //local_start.x = int ( global_points[global_path[i]].x/ pixel_distance );
-                //local_start.y = int ( global_points[global_path[i]].y / pixel_distance );
-                //local_goal.x = int ( global_points[global_path[i-1]].x / pixel_distance );
-                //local_goal.y = int ( global_points[global_path[i-1]].y / pixel_distance );
-
-
-                //cout << local_start.x << " " << local_start.y << " " << local_goal.x << " " << local_goal.y << endl;
-                //vector<point> local_path; 
-                //local_path.push_back(local_start); 
-                //point rand_point = random();
-/*
-                for (int i = 0; i < local_path.size(); ++i){
-                    double min_dis = 1000000000.0;
-                    point* nearest_point;
-                    if ( distance(rand_point, local_path[i]) != 0 ){
-                        if ( distance(rand_point, local_path[i]) < min_dis){
-                            nearest_point = &(local_path[i]);
-                        }
-                    }
-
-                    cout << nearest_point->x << " " << nearest_point->y << endl;
-
-                }
-
-*/
-
-//            }
+            for (int i = 0; i < 16; ++i){
+                global_points[i].obstacle = msg.data[i]; 
+            }
             
-        }
-
-        point random(){
-            srand((unsigned int)time(0));
-            point temp;
-            temp.x = rand() % h;
-            temp.y = rand() % h;
-            cout << temp.x << " " << temp.y << endl;
-            return temp;
-        }
-
-        double distance(point p1, point p2){
-            return pow(pow((p1.x - p2.x), 2) + pow((p1.y - p2.y), 2) , 0.5) ;
         }
 
 };
@@ -276,7 +197,8 @@ int main(int argc, char *argv[]){
     Planning planning;
 
     while (ros::ok()){
-        planning.rrt_star();
+
+        planning.publish_path();
         ros::spinOnce();
         loop_rate.sleep();
     }

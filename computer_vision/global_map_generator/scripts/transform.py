5583878e7ac7de272feba6d45ad9a99ee0e7e6ce
Ayoung Lee <aylee2008@naver.com>
2020-11-17 13:13:44 +0900

    modified perspective matrix

--- computer_vision/global_map_generator/scripts/transform.py
+++ computer_vision/global_map_generator/scripts/transform.py
@@ -2,41 +2,47 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img_middle = cv2.imread('/home/snuzero/catkin_ws/src/zero_maker/computer_vision/global_map_generator/img_sample/cam1/point_label_img_00005.jpg')
-pts1 = np.float32([[88, 57], [320, 18], [551, 59], [320, 124]])
-pts2 = np.float32([[1016, 879], [508, 1172], [0, 879], [508, 586]])
+img_middle = cv2.imread('/home/ayoung/catkin_ws/src/zero_maker/computer_vision/global_map_generator/img_sample/cam1/point_label_img_00005.jpg')
+pts1 = np.float32([[88, 58], [320, 18], [552, 58], [320, 124]])
+pts2 = np.float32([[1040, 900], [520, 1200], [0, 900], [520, 600]])
 
 M = cv2.getPerspectiveTransform(pts1, pts2)
 print("middle:")
 print(M)
 
-dst = cv2.warpPerspective(img_middle, M, (1016,1172))
+dst_middle = cv2.warpPerspective(img_middle, M, (1040,1200))
 
-plt.imshow(dst)
+plt.imshow(dst_middle)
 plt.show()
 
-img_right = cv2.imread('/home/snuzero/catkin_ws/src/zero_maker/computer_vision/global_map_generator/img_sample/cam2/point_label_img_00006.jpg')
-pts1 = np.float32([[87, 54], [315, 17], [540, 55], [313, 118]])
-pts2 = np.float32([[508, 0], [1016, 300], [1016, 879], [508, 586]])
+img_right = cv2.imread('/home/ayoung/catkin_ws/src/zero_maker/computer_vision/global_map_generator/img_sample/cam2/point_label_img_00006.jpg')
+pts1 = np.float32([[88, 53], [313, 15], [539, 53], [311, 116]])
+pts2 = np.float32([[520, 0], [1040, 300], [1040, 900], [520, 600]])
 
 M = cv2.getPerspectiveTransform(pts1, pts2)
 print("right:")
 print(M)
 
-dst = cv2.warpPerspective(img_right, M, (1016,1172))
+dst_right = cv2.warpPerspective(img_right, M, (1040,1200))
 
-plt.imshow(dst)
+plt.imshow(dst_right)
 plt.show()
 
-img_left = cv2.imread('/home/snuzero/catkin_ws/src/zero_maker/computer_vision/global_map_generator/img_sample/cam3/point_label_img_00005.jpg')
-pts1 = np.float32([[100, 55], [324, 17], [551, 54], [327, 118]])
-pts2 = np.float32([[0, 879], [0, 300], [508, 0], [508, 586]])
+img_left = cv2.imread('/home/ayoung/catkin_ws/src/zero_maker/computer_vision/global_map_generator/img_sample/cam3/point_label_img_00005.jpg')
+pts1 = np.float32([[98, 54], [323, 16], [551, 54], [325, 118]])
+pts2 = np.float32([[0, 900], [0, 300], [520, 0], [520, 600]])
 
 M = cv2.getPerspectiveTransform(pts1, pts2)
 print("left:")
 print(M)
 
-dst = cv2.warpPerspective(img_left, M, (1016,1172))
+dst_left = cv2.warpPerspective(img_left, M, (1040,1200))
 
-plt.imshow(dst)
+plt.imshow(dst_left)
+plt.show()
+
+bit_and_tmp = cv2.bitwise_and(dst_middle, dst_left)
+bit_and = cv2.bitwise_and(dst_right, bit_and_tmp)
+
+plt.imshow(bit_and)
 plt.show()
